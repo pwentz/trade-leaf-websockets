8f72be30a41957f6b021d73b7a4b5add521d885d
pwentz <patrick.w.wentz@gmail.com>
2018-05-19 18:46:34 -0500

    - change format of error messages - logs to std out

--- app/Server.hs
+++ app/Server.hs
@@ -11,18 +11,14 @@ import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import qualified MessageParser as MP
 import qualified Data.Map as Map
+import Control.Monad.IO.Class (liftIO)
 
 import qualified Network.WebSockets as WS
-
--- We represent a client by their username and a `WS.Connection`. We will see how we
--- obtain this `WS.Connection` later on.
 
 type ServerState = Map.Map Int WS.Connection
 
 newServerState :: ServerState
 newServerState = Map.empty
-
--- Check if a user already exists (based on username):
 
 acceptableRequest :: WS.AcceptRequest
 acceptableRequest =
@@ -34,29 +30,10 @@ addClient = Map.insert
 removeClient :: Int -> ServerState -> ServerState
 removeClient = Map.delete
 
-
--- The main function first creates a new state for the server, then spawns the
--- actual server. For this purpose, we use the simple server provided by
--- `WS.runServer`.
-
 main :: IO ()
 main = do
     state <- newMVar newServerState
     WS.runServer "127.0.0.1" 9160 $ application state
-
--- Our application starts by accepting the connection. In a more realistic
--- application, you probably want to check the path and headers provided by the
--- pending request.
-
--- We also fork a pinging thread in the background. This will ensure the connection
--- stays alive on some browsers.
-
--- When a client is succesfully connected, we read the first message. This should
--- be in the format of "Hi! I am Jasper", where Jasper is the requested username.
-
--- We send a "Welcome!", according to our own little protocol. We add the client to
--- the list and broadcast the fact that he has joined. Then, we give control to the
--- 'talk' function.
 
 application :: MVar ServerState -> WS.ServerApp
 application state pending = do
@@ -66,8 +43,9 @@ application state pending = do
     clients <- readMVar state
     case MP.parseNewUser (T.unpack msg) of
       Nothing ->
-        WS.sendTextData conn ("-- Invalid format" :: Text)
-      Just newUser ->
+        WS.sendTextData conn ("-- INVALID FORMAT" :: Text)
+      Just newUser -> do
+        putStrLn (mconcat ["NEW USER: ", show newUser])
         flip finally disconnect $ do
            modifyMVar_ state (return . addClient newUser conn )
            talk conn state newUser
@@ -78,20 +56,17 @@ application state pending = do
                   in return (s', s')
               return ()
 
--- The talk function continues to read messages from a single client until he
--- disconnects. All messages are broadcasted to the other clients.
-
 sendTo :: Text -> WS.Connection -> Int -> ServerState -> IO ()
 sendTo msg conn recipientId state =
   case Map.lookup recipientId state of
-    Nothing -> WS.sendTextData conn (mconcat ["-- ", T.pack $ show recipientId, " has disconnected"])
+    Nothing -> WS.sendTextData conn (mconcat ["-- ", T.pack $ show recipientId, ": DISCONNECTED"])
     Just receivingConn ->
-      WS.sendTextData receivingConn msg
+      WS.sendTextData receivingConn (mconcat [T.pack $ show recipientId, ": ", msg])
 
 talk :: WS.Connection -> MVar ServerState -> Int -> IO ()
 talk conn state userId = forever $ do
-  -- listen for new message
     msg <- WS.receiveData conn
+    putStrLn (mconcat [show userId, ": ", T.unpack msg])
     case MP.parseMessage (T.unpack msg) of
       Nothing -> return ()
       Just (recipientId, message) ->
